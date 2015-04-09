0c4183bc967207bd60ad2c5f675f0f1e3f57d48b
pomme <apple.chan-fardel@guardian.co.uk>
2015-04-09 15:08:29 +0100

    link components dropdown and expand

---
+++ src/js/common/utilities.js
@@ -0,0 +1,22 @@
+define([
+], function(
+) {
+    'use strict';
+
+    // utilities: string 
+    function isPattern(string, pattern) {
+        return string.indexOf(pattern) > -1;
+    }
+    function removePattern(string, pattern) {
+        return string.replace(pattern, "");
+    }
+    function capitalizeFirstLetter(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+
+    return {
+        isPattern: isPattern,
+        removePattern: removePattern,
+        capitalizeFirstLetter: capitalizeFirstLetter
+    };
+});
--- src/js/page/page.js
+++ src/js/page/page.js
@@ -1,15 +1,17 @@
 define([
+    'common/utilities'
 ], function(
+    util
 ) {
     'use strict';
     
     function render(data) {
-        /* date */
+        /* data */
         var dataMap = data.filter(function(d) {
-            return isPattern(d.component, "map-");
+            return util.isPattern(d.component, "map-");
         }).map(function(d) {
             // add html ids to data
-            d.id = "js" + capitalizeFirstLetter(removePattern(d.component, "map-"));
+            d.id = "js" + util.capitalizeFirstLetter(util.removePattern(d.component, "map-"));
             return d;
         });
         //console.log(dataMap);
@@ -22,39 +24,7 @@ define([
                 doc.querySelector("#" + d.id + " .js-gloss").textContent = d.gloss;
             }
         });
-
-        /* component */
-        doc.querySelector(".js-btn-expand").addEventListener("click", onExpand, false);
-        doc.querySelector(".js-btn-collapse").addEventListener("click", onExpand, false);
     }
-
-
-    // utilities: string 
-    function isPattern(string, pattern) {
-        return string.indexOf(pattern) > -1;
-    }
-    function removePattern(string, pattern) {
-        return string.replace(pattern, "");
-    }
-    function capitalizeFirstLetter(string) {
-        return string.charAt(0).toUpperCase() + string.slice(1);
-    }
-
-
-    // component: expand or collapse
-    function onExpand() {
-        var el = document.querySelector(".js-expand"),
-            cn = el.className;
-        
-        if (isPattern(cn, "hide")) {
-            el.className = removePattern(cn, " hide") + " show"; 
-        } else if (isPattern(cn, "show")) {
-            el.className = removePattern(cn, " show") + " hide"; 
-        }
-    }
-    // component: social?
-    // ...
-
 
     return {
         render: render
