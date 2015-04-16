d47008a0e4f3e698c845c06dd23462072504848c
pomme <apple.chan-fardel@guardian.co.uk>
2015-04-16 13:47:13 +0100

    remove nav tabs at 660px

--- src/js/common/elementStick.js
+++ src/js/common/elementStick.js
@@ -5,14 +5,23 @@ define([
 ) {
     'use strict';
 
-    function getOffsetTop (el) {
+    function isTab () {
+        var d = document,
+            e = d.documentElement,
+            g = d.getElementsByTagName('body')[0],
+            w = window.innerWidth || e.clientWidth || g.clientWidth;
+        return w > 660;
+    }
+
+    function getOffsetTop (el, isTab) {
         //TODO: if more than one parent has offsetTop
         var f = document.querySelector("#header"),
-            p = el.parentNode,
             top  = el.offsetTop;
-        if (f !== null) { top += 179; } //remove fix number: ad + nav g! 
-        if (p !== null) { top += 79; }  //remove fix number: nav tabs
+
+        if (f !== null) { top += 179; } //remove magic number: ad + nav g! 
+        top += isTab ? 79 : 30;         //remove magic number: nav tabs
         /*
+        console.log(isTab);
         console.log(el);
         console.log(el.offsetTop);
         console.log(el.offsetParent);
@@ -24,13 +33,13 @@ define([
 
     function stickElementOnScroll() {
         var el = document.querySelector("#jsSticky"),
-            offset = getOffsetTop(el);
-        
+            offset = getOffsetTop(el, isTab());
+
         function stickIfNeeded() {
             var cn = el.className,
                 cnSticky = "sticky",
                 pageOffset = (document.documentElement && document.documentElement.scrollTop) || 
-                              document.body.scrollTop; //window.pageYOffset
+                document.body.scrollTop; //window.pageYOffset
             if (offset <= pageOffset) {
                 cn  = cn + (util.isPattern(cn, cnSticky) ? "" : " " + cnSticky);
             } else {
@@ -39,14 +48,13 @@ define([
             el.className = cn.trim();
         }
 
-        function getOffsetAndStickIfNeeded() {
-            offset = getOffsetTop(el);
-            stickIfNeeded();
+        function setOffsetTop() {
+            offset = getOffsetTop(el, isTab());
         }
 
         //addEventListener
         window.addEventListener("scroll", util.throttle(stickIfNeeded, 300));
-        window.addEventListener("resize", util.throttle(getOffsetAndStickIfNeeded, 300));
+        window.addEventListener("resize", util.throttle(setOffsetTop, 300));
     }
 
     return {
